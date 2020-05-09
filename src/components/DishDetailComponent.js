d55523e695dd1ec30c99dc24997f820a26eb528c
Rahul Jain <rahul247jain@gmail.com>
2020-05-09 13:51:27 +0530

    Assignment 3 Solution

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,60 +1,63 @@
-import React from 'react';
-import { Card, CardImg, CardText, CardBody,
-  CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
-import { Link } from 'react-router-dom';
-  
+import React from "react";
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  Breadcrumb,
+  BreadcrumbItem,
+} from "reactstrap";
+import { Link } from "react-router-dom";
+import CommentForm from "./CommentForm";
 
-  function RenderDish({dish}){
-      if(dish!=null){
-             return(
-              
-               <div className="col-12 col-md-5 m-1">
-                   <Card>
-                     <CardImg width="100%" src={dish.image} alt={dish.name} />
-                     <CardBody>
-                       <CardTitle>{dish.name}</CardTitle>
-                       <CardText>{dish.description}</CardText>
-                     </CardBody>
-                   </Card>
-                </div>
-                
-             );}
-      else {
-        return(
-          <div></div>
-        );}
-   }
-
-  function RenderComments({comments}){
-
-    if(comments!=null){
-  return( 
-<div className="col-12 col-md-5 m-1">
-         <h4>Comments</h4>
-             {comments.map(comment => {
-          return (
-              <li key={comment.id}>
-                <p>{comment.comment}</p>
-                <p>
-                  -- {comment.author} ,{" "}
-                  {new Intl.DateTimeFormat("en-US", {
-                    year: "numeric",
-                    month: "short",
-                    day: "2-digit"
-                  }).format(new Date(Date.parse(comment.date)))}
-                </p>
-              </li>
-          );
-        })}
-        </div>
-    );    
-  } else return <div />;    
+function RenderDish({ dish }) {
+  if (dish != null) {
+    return (
+      <div className="col-12 col-md-5 m-1">
+        <Card>
+          <CardImg width="100%" src={dish.image} alt={dish.name} />
+          <CardBody>
+            <CardTitle>{dish.name}</CardTitle>
+            <CardText>{dish.description}</CardText>
+          </CardBody>
+        </Card>
+      </div>
+    );
+  } else {
+    return <div></div>;
+  }
 }
 
-  const DishDetail= (props) => {
+function RenderComments({ comments }) {
+  if (comments != null) {
+    return (
+      <div className="col-12 col-md-5 m-1">
+        <h4>Comments</h4>
+        {comments.map((comment) => {
+          return (
+            <li key={comment.id}>
+              <p>{comment.comment}</p>
+              <p>
+                -- {comment.author} ,{" "}
+                {new Intl.DateTimeFormat("en-US", {
+                  year: "numeric",
+                  month: "short",
+                  day: "2-digit",
+                }).format(new Date(Date.parse(comment.date)))}
+              </p>
+            </li>
+          );
+        })}
+        <CommentForm />
+      </div>
+    );
+  } else return <div />;
+}
 
-    if (props.dish != null){
-      return (
+const DishDetail = (props) => {
+  if (props.dish != null) {
+    return (
       <div className="container">
         <div className="row">
           <Breadcrumb>
@@ -74,8 +77,7 @@ import { Link } from 'react-router-dom';
         </div>
       </div>
     );
-    }
-   
-  };
+  }
+};
 
-export default DishDetail; 
+export default DishDetail;
